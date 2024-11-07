03dfe9d1569d38df15cf04525948dc77b578894f
VIVALDI142857 <gleb.trofimov.07@mail.ru>
2024-11-07 15:42:56 -0300

    update

---
+++ botstudy.py
@@ -0,0 +1,19 @@
+import aiogram 
+from aiogram import Bot, Dispatcher, types
+import asyncio
+
+token: str = '7280734429:AAHaUwW9B6aqRdygZak0U14KLl1NX5OG1is'
+
+bot = Bot(token)
+dp = Dispatcher()
+
+@dp.message_handler()
+
+async def hello():
+    print('Hello, Welcome to SENATOROV Education!')
+
+if__name__ == '__main__'
+
+git config --global user.name "VIVALDI142857"
+
+git config --global user.email "gleb.trofimov.07@mail.ru"
