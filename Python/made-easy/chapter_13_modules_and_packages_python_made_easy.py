fdffba5bbb5ec5b3a1854a95ff587558c5917dcf
Ruslan Senatorov <55090151+ruslansenatorov@users.noreply.github.com>
2024-11-08 12:20:38 -0800

    test

--- Python/made-easy/chapter_13_modules_and_packages_python_made_easy.py
+++ Python/made-easy/chapter_13_modules_and_packages_python_made_easy.py
@@ -1,16 +1,3 @@
-# ---
-# jupyter:
-#   jupytext:
-#     text_representation:
-#       extension: .py
-#       format_name: light
-#       format_version: '1.5'
-#       jupytext_version: 1.16.4
-#   kernelspec:
-#     display_name: base
-#     language: python
-#     name: python3
-# ---
 """Глава 13."""
 
 # # Модули
