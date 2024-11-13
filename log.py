a86dd1c44bc5283b4dc6e2f298d389cc026257be
Ruslan Senatorov <55090151+ruslansenatorov@users.noreply.github.com>
2024-11-14 01:37:14 +0300

    Update log.py
    Signed-off-by: Ruslan Senatorov <55090151+ruslansenatorov@users.noreply.github.com>

--- log.py
+++ log.py
@@ -5,8 +5,7 @@ import numpy as np
 import scipy as sc
 import scipy.special as scs
 import seaborn as sns
- ыфввфвфвф вфвь фцвawdasda
-awd
+ 
 # # 24.08
 # Вводный урок, начали разбирать комбинаторику:
 #
